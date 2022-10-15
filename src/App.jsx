58651eed1fcbcf036cd870c616ea693259af9ad4
franjsb1903 <franchu.besteiro@gmail.com>
2022-10-15 18:08:28 +0200

    Create form and list
    Created form and list of patients

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import { useState } from 'react'
 import Formulario from './components/Formulario'
 import Header from './components/Header'
 import ListadoPacientes from './components/ListadoPacientes'
 
 function App() {
+  const [pacientes, setPacientes] = useState([])
+
+  const addPaciente = paciente => {
+    setPacientes([paciente, ...pacientes])
+  }
+
   return (
     <div className="container mx-auto mt-20">
       <Header />
-      <Formulario />
-      <ListadoPacientes />
+      <div className="mt-12 md:flex">
+        <Formulario addPaciente={addPaciente} />
+        <ListadoPacientes />
+      </div>
     </div>
   )
 }
