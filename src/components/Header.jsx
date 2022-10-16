b7778c3a2a8b7a2ba61847dfc0322b3e5c69a9a1
franjsb1903 <franchu.besteiro@gmail.com>
2022-10-16 13:47:20 +0200

    Changed header for CI/CD

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 const Header = () => {
   return (
     <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-      Seguimiento pacientes <span className="text-indigo-600">Veterinaria</span>
+      Seguimiento pacientes con GitHub{' '}
+      <span className="text-indigo-600">Veterinaria</span>
     </h1>
   )
 }
